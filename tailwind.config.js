07275f2175b05fef0fa30f0d7603d46f264db45e
Rickyngechu <ngerick00@gmail.com>
2024-02-23 23:40:21 +0300

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      "Dark-Blue": "hsl(209, 23%, 22%)",
+      "Very-Dark-Blue": "hsl(207, 26%, 17%)",
+      "Very-Dark-Blue": "hsl(200, 15%, 8%)",
+      "Dark-Gray": "hsl(0, 0%, 52%)",
+      "Very-Light-Gray": "hsl(0, 0%, 98%)",
+      White: "hsl(0, 0%, 100%)",
+    },
     extend: {},
   },
   plugins: [],
