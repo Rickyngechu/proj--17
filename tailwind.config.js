e37b53557cec9b54626922788f7e27961559bd04
Rickyngechu <ngerick00@gmail.com>
2024-02-24 12:25:44 +0300

    Markup finalizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,19 @@ module.exports = {
   theme: {
     colors: {
       "Dark-Blue": "hsl(209, 23%, 22%)",
-      "Very-Dark-Blue": "hsl(207, 26%, 17%)",
-      "Very-Dark-Blue": "hsl(200, 15%, 8%)",
+      "Very-Dark-Blue1": "hsl(207, 26%, 17%)",
+      "Very-Dark-Blue2": "hsl(200, 15%, 8%)",
       "Dark-Gray": "hsl(0, 0%, 52%)",
       "Very-Light-Gray": "hsl(0, 0%, 98%)",
       White: "hsl(0, 0%, 100%)",
     },
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Custom grids
+        country: "repeat(auto-fit, minmax(20rem, 1fr))",
+        "country-details": "repeat(auto-fit, minmax(25rem, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
