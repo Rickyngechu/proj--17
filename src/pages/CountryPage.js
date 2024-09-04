10d3ffc4c7035318c1f88337e61f5a9504873c9a
Erick ngechu <ekngechu003@gmail.com>
2024-09-04 23:53:19 +0300

    Fixed Eslint errors

--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Loader from "../components/Loader";
 
 function CountryPage() {
